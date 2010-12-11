9185a4a316254a9962a07c370fed5996c2dbc68a
François Poirotte <clicky@erebot.net>
2010-12-11 17:29:25 +0000

    Removed old autoloader. Removed dependency on that autoloader in the different build.xml files. Fixed unit tests to always require_once testenv/bootstrap.php. Added the testenv to svn:externals where needed.

--- tests/CardTest.php
+++ tests/CardTest.php
@@ -16,6 +16,12 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+require_once(
+    dirname(__FILE__) .
+    DIRECTORY_SEPARATOR . 'testenv' .
+    DIRECTORY_SEPARATOR . 'bootstrap.php'
+);
+
 class   Erebot_Module_GoF_CardTest
 extends PHPUnit_Framework_TestCase
 {
